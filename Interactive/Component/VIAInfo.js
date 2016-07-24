a21c21e2e41e0eda52c21661f96404d5c1bb3ba7
joshuaprewitt <joshua.prewitt@ni.com>
2016-07-24 08:58:34 -0500

    Added multiple plots to the chart and the ability to reset the aggregates

--- Interactive/Component/VIAInfo.js
+++ Interactive/Component/VIAInfo.js
@@ -4,6 +4,7 @@ NationalInstruments.HtmlVI.viName = '_%46unction%2Egvi';
 //inputs: 
 NationalInstruments.HtmlVI.inputDataItem = {};
 NationalInstruments.HtmlVI.inputDataItem['di_39']='Boolean';
+NationalInstruments.HtmlVI.inputDataItem['di_974']='Boolean';
 
 //outputs:
 NationalInstruments.HtmlVI.outputDataItem = {};
@@ -11,5 +12,9 @@ NationalInstruments.HtmlVI.outputDataItem['di_780']='Number';
 NationalInstruments.HtmlVI.outputDataItem['di_779']='Number';
 NationalInstruments.HtmlVI.outputDataItem['di_772']='Number';
 NationalInstruments.HtmlVI.outputDataItem['di_765']='Number';
-NationalInstruments.HtmlVI.outputDataItem['di_702']='Number';
+NationalInstruments.HtmlVI.outputDataItem['di_1025']='Number';
+NationalInstruments.HtmlVI.outputDataItem['di_1020']='Number';
+NationalInstruments.HtmlVI.outputDataItem['di_1030']='Number';
+NationalInstruments.HtmlVI.outputDataItem['di_702']='Array';
 NationalInstruments.HtmlVI.outputDataItem['di_626']='Boolean';
+NationalInstruments.HtmlVI.outputDataItem['di_975']='Boolean';
