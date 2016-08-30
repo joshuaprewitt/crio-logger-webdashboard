d3cd10c5e35bea273bf199ae7ee092543cd2d8c6
joshuaprewitt <joshua.prewitt@ni.com>
2016-08-30 12:31:53 -0500

    Updated URL for the new server

--- Interactive/Component/VIAInfo.js
+++ Interactive/Component/VIAInfo.js
@@ -8,10 +8,10 @@ NationalInstruments.HtmlVI.inputDataItem['di_974']='Boolean';
 
 //outputs:
 NationalInstruments.HtmlVI.outputDataItem = {};
+NationalInstruments.HtmlVI.outputDataItem['di_626']='Boolean';
+NationalInstruments.HtmlVI.outputDataItem['di_975']='Boolean';
 NationalInstruments.HtmlVI.outputDataItem['di_780']='Number';
 NationalInstruments.HtmlVI.outputDataItem['di_779']='Number';
 NationalInstruments.HtmlVI.outputDataItem['di_772']='Number';
 NationalInstruments.HtmlVI.outputDataItem['di_765']='Number';
 NationalInstruments.HtmlVI.outputDataItem['di_702']='Array';
-NationalInstruments.HtmlVI.outputDataItem['di_626']='Boolean';
-NationalInstruments.HtmlVI.outputDataItem['di_975']='Boolean';
